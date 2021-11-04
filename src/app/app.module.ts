4a63f9d71f745105e1e86f25935f788e526dcee2
Juan Antonio Pereiro <Pereiro92@gmail.com>
2021-11-04 20:32:34 +0100

    core components like sidenav, header, footer have been added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { AppComponent } from './core/components/base/app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    CoreModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { AppComponent } from './components/base/app.component';
+import { RouterModule } from '@angular/router';
+import { MaterialsModule } from './modules/materials.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+  ],
+  exports: [],
+  imports: [CommonModule, RouterModule, MaterialsModule],
 })
-export class CoreModule { }
+export class CoreModule {}
